46c1b0327b61aa915fb21eeeb141fa5d46425605
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2024-08-15 14:32:31 +0900

    ble: gap: add functions (ScanEnable, AdvReport, ConnectionComplete, ChannelSelAlg).
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- src/nim_nucleus/ble/ble_gap.nim
+++ src/nim_nucleus/ble/ble_gap.nim
@@ -1,16 +1,25 @@
 import std/asyncdispatch
+import std/options
+import std/strformat
 import ./ble_client
+import ./core/hci_status
 import ./core/opc
 import ./gap/types
 import ./util
+import ../lib/syslog
 export types
+
+# ==============================================================================
+# Requests
+# ==============================================================================
 
 # ------------------------------------------------------------------------------
 # 1.2.11 LE Set Scan Parameters 要求
 # ------------------------------------------------------------------------------
-proc setScanParametersReq*(self: BleClient, scanType: ScanType, scanInterval: uint16,
-    scanWindow: uint16, ownAddrType: AddrType, ownRandomAddrType: RandomAddrType,
-    filterPolicy: ScanFilterPolicy): Future[bool] {.async.} =
+proc setScanParametersReq*(self: BleClient, scanType: ScanType, scanInterval: uint16 = 0x0010,
+    scanWindow: uint16 = 0x0010, ownAddrType: AddrType, ownRandomAddrType: RandomAddrType,
+    filterPolicy: ScanFilterPolicy = ScanFilterPolicy.AcceptAllExceptDirected):
+    Future[bool] {.async.} =
   const
     procName = "setScanParametersReq"
     reqOpc = BTM_D_OPC_BLE_GAP_SET_SCAN_PARAMETERS_REQ
@@ -24,3 +33,90 @@ proc setScanParametersReq*(self: BleClient, scanType: ScanType, scanInterval: ui
   buf[8] = ownRandomAddrType.uint8
   buf[9] = filterPolicy.uint8
   result = await self.btmRequest(procName, buf.toString, expOpc)
+
+# ------------------------------------------------------------------------------
+# 1.2.13 LE Set Scan Enable 要求
+# ------------------------------------------------------------------------------
+proc setScanEnableReq*(self: BleClient, scanEnable: bool, filterDuplicates: bool = true):
+    Future[bool] {.async.} =
+  const
+    procName = "setScanParametersReq"
+    reqOpc = BTM_D_OPC_BLE_GAP_SET_SCAN_ENABLE_REQ
+    expOpc = BTM_D_OPC_BLE_GAP_SET_SCAN_ENABLE_RSP
+  var buf: array[4, uint8]
+  buf.setOpc(0, reqOpc)
+  buf[2] = (if scanEnable: Scan.Enable else: Scan.Disable).uint8
+  buf[3] = (if filterDuplicates: DuplicacaFilter.Enable
+      else: DuplicacaFilter.Disable).uint8
+  result = await self.btmRequest(procName, buf.toString, expOpc)
+
+# ==============================================================================
+# Event Parsers
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# 1.2.15 LE Advertising Report 通知
+# ------------------------------------------------------------------------------
+proc parseAdvertisingReport*(self: BleClient, payload: string): Option[AdvertisingReport] =
+  const procName = "parseAdertisingReport"
+  if not checkPayloadLen(procName, payload, 43):
+    return
+  try:
+    var res: AdvertisingReport
+    res.eventType = payload[2].EventType
+    res.addrType = payload[3].DirectAddrType
+    res.bdAddr = payload.getBdAddr(4)
+    let dataLen = payload[10].uint8
+    res.data = newString(dataLen)
+    copyMem(addr res.data[0], addr payload[11], dataLen)
+    res.rssi = payload[42].int8
+    result = some(res)
+  except:
+    let err = getCurrentExceptionMsg()
+    let errmsg = &"! {procName}: caught exception, {err}"
+    syslog.error(errmsg)
+
+# ------------------------------------------------------------------------------
+# 1.2.71 LE Enhanced Connection Complete 通知
+# ------------------------------------------------------------------------------
+proc parseEnhConnectionComplete*(self: BleClient, payload: string):
+    Option[EnhConnectionCompleteEvent] =
+  const procName = "parseEnhConnectionComplete"
+  if not checkPayloadLen(procName, payload, 32):
+    return
+  try:
+    var res: EnhConnectionCompleteEvent
+    res.hciStatus = payload[2].HciStatus
+    res.conHandle = payload.getLe16(3)
+    res.role = payload[5].Role
+    res.peerAddrType = payload[6].DirectAddrType
+    res.peerAddr = payload.getBdAddr(7)
+    res.localPrivateAddr = payload.getBdAddr(13)
+    res.remotePrivateAddr = payload.getBdAddr(19)
+    res.conInterval = payload.getLe16(25)
+    res.conLatency = payload.getLe16(27)
+    res.supervisionTimeout = payload.getLe16(29)
+    res.masterClockAccuracy = payload[31].ClockAccuracy
+    result = some(res)
+  except:
+    let err = getCurrentExceptionMsg()
+    let errmsg = &"! {procName}: caught exception, {err}"
+    syslog.error(errmsg)
+
+# ------------------------------------------------------------------------------
+# 1.2.78 LE Channel Selection Algorithm 通知
+# ------------------------------------------------------------------------------
+proc parseChannelSelAlgorithm*(self: BleClient, payload: string):
+    Option[ChannelSelAlgorithmReport] =
+  const procName = "parseChannelSelAlgorithm"
+  if not checkPayloadLen(procName, payload, 5):
+    return
+  try:
+    var res: ChannelSelAlgorithmReport
+    res.conHandle = payload.getLe16(2)
+    res.alg = payload[4].ChannSelAlgorithm
+    result = some(res)
+  except:
+    let err = getCurrentExceptionMsg()
+    let errmsg = &"! {procName}: caught exception, {err}"
+    syslog.error(errmsg)
--- src/nim_nucleus/ble/gap/types.nim
+++ src/nim_nucleus/ble/gap/types.nim
@@ -1,3 +1,6 @@
+import std/options
+import ../core/hci_status
+
 type
   # Set Advertising Parameter Request (0x4000)
   AdvType* {.pure.} = enum
@@ -46,3 +49,51 @@ type
   DuplicacaFilter* {.pure.} = enum
     Disable = 0x00'u8
     Enable = 0x01'u8
+
+type
+  EventType* {.pure.} = enum
+    IND = 0x00
+    DIRECT_IND = 0x01
+    SCAN_IND = 0x02
+    NONCONN_IND = 0x03
+    SCAN_RSP = 0x04
+  Role* {.pure.} = enum
+    Central = 0x00'u8
+    Peripheral = 0x01'u8
+  ClockAccuracy* = enum
+    acc500ppm = (0x00'u8, "500ppm")
+    acc250ppm = (0x01'u8, "250ppm")
+    acc150ppm = (0x02'u8, "150ppm")
+    acc100ppm = (0x03'u8, "100ppm")
+    acc75ppm = (0x04'u8, "75ppm")
+    acc50ppm = (0x05'u8, "50ppm")
+    acc30ppm = (0x06'u8, "30ppm")
+    acc25ppm = (0x07'u8, "25ppm")
+  ChannSelAlgorithm* {.pure.} = enum
+    alg0 = (0x00'u8, "LE Channel Selection Algorithm #1")
+    alg1 = (0x01'u8, "LE Channel Selection Algorithm #2")
+  # LE Advertising Report (0x4017)
+  AdvertisingReport* = object
+    eventType*: EventType
+    addrType*: DirectAddrType
+    bdAddr*: uint64
+    name*: Option[string]
+    data*: string
+    rssi*: int8
+  # LE Enhanced Connection Complete 通知 (0x419f)
+  EnhConnectionCompleteEvent* = object
+    hciStatus*: HciStatus
+    conHandle*: uint16
+    role*: Role
+    peerAddrType*: DirectAddrType
+    peerAddr*: uint64
+    localPrivateAddr*: uint64
+    remotePrivateAddr*: uint64
+    conInterval*: uint16
+    conLatency*: uint16
+    supervisionTImeout*: uint16
+    masterClockAccuracy*: ClockAccuracy
+  # LE Enhanced Connection Complete 通知 (0x419f)
+  ChannelSelAlgorithmReport* = object
+    conHandle*: uint16
+    alg*: ChannSelAlgorithm
