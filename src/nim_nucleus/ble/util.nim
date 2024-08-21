63fbd2f2230676ef28f4f491be736c3eb69a0061
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2024-08-21 10:03:15 +0900

    ble: util: fix set/get BD address, etc.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- src/nim_nucleus/ble/util.nim
+++ src/nim_nucleus/ble/util.nim
@@ -1,4 +1,5 @@
-import std/nativesockets
+import std/strformat
+import std/strutils
 
 # ------------------------------------------------------------------------------
 #
@@ -22,21 +23,9 @@ func lb(x: uint16): uint8 {.inline.} =
 # ------------------------------------------------------------------------------
 #
 # ------------------------------------------------------------------------------
-func be_hb(x: uint16): uint8 {.inline.} =
-  result = x.htons.hb
-
-# ------------------------------------------------------------------------------
-#
-# ------------------------------------------------------------------------------
-func be_lb(x: uint16): uint8 {.inline.} =
-  result = x.htons.lb
-
-# ------------------------------------------------------------------------------
-#
-# ------------------------------------------------------------------------------
 proc setOpc*(buf: var openArray[uint8|char], pos: int, opc: uint16) {.inline.} =
-  buf[pos] = opc.be_hb
-  buf[pos + 1] = opc.be_lb
+  buf[pos] = opc.hb
+  buf[pos + 1] = opc.lb
 
 # ------------------------------------------------------------------------------
 #
@@ -66,22 +55,35 @@ proc getLeInt16*(buf: openArray[uint8|char]|string, pos: int): int16 {.inline.} 
 # ------------------------------------------------------------------------------
 #
 # ------------------------------------------------------------------------------
+proc getU8*(buf: openArray[uint8|char]|string, pos: int): uint8 {.inline.} =
+  result = cast[uint8](buf[pos])
+
+# ------------------------------------------------------------------------------
+#
+# ------------------------------------------------------------------------------
+proc getS8*(buf: openArray[uint8|char]|string, pos: int): int8 {.inline.} =
+  result = cast[int8](buf[pos])
+
+# ------------------------------------------------------------------------------
+#
+# ------------------------------------------------------------------------------
 proc setBdAddr*(buf: var openArray[uint8|char], pos: int, bdAddr: uint64) =
   for idx in 0 ..< 6:
     let octet = ((bdAddr shr (idx * 8)) and 0xff).uint8
-    buf[idx] = octet
+    buf[pos + idx] = octet
 
 # ------------------------------------------------------------------------------
 #
 # ------------------------------------------------------------------------------
 proc getBdAddr*(buf: openArray[uint8|char]|string, pos: int): uint64 =
   for idx in 0 ..< 6:
-    result = result or (buf[idx].uint64 shl (idx * 8))
+    result = result or (buf[pos + idx].uint64 shl (idx * 8))
 
 # ------------------------------------------------------------------------------
 #
 # ------------------------------------------------------------------------------
-proc setLeArray*(buf: var openArray[uint8|char], pos: int, val: openArray[uint8], len: int) =
+proc setLeArray*(buf: var openArray[uint8|char], pos: int, val: openArray[uint8],
+    len: int) =
   for idx in 0 ..< len:
     buf[pos + idx] = val[idx]
 
@@ -100,3 +102,13 @@ proc getLeArray*(src: openArray[uint8|char]|string, dest: var openArray[uint8],
     pos: int, len: int) =
   for idx in 0 ..< len:
     dest[idx] = src[pos + idx].uint8
+
+# ------------------------------------------------------------------------------
+#
+# ------------------------------------------------------------------------------
+proc bdAddr2string*(x: uint64): string =
+  var octets: array[6, string]
+  for idx in 0 ..< 6:
+    let octet = ((x shr (idx * 8)) and 0xff).uint8
+    octets[5 - idx] = &"{octet:02X}"
+  result = octets.join(":")
