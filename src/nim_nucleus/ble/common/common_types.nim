73e5aed4ac24892bc0ab1b1da47873f73151e96a
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2024-08-23 10:03:11 +0900

    ble: common: common_types: Move AddressType-related object definitions here.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- src/nim_nucleus/ble/common/common_types.nim
+++ src/nim_nucleus/ble/common/common_types.nim
@@ -2,3 +2,9 @@ type
   AddrType* {.pure.} = enum
     Public = 0x00'u8
     Random = 0x01'u8
+  PeerAddr* = object
+    addrType*: AddrType
+    address*: uint64
+  LocalAddr* = object
+    addrType*: AddrType
+    address*: uint64
