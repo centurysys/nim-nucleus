2ceb7171c4fbd230724e5e278fac8c96ac08a226
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2024-08-23 10:13:04 +0900

    nim_nucleus: add submodule definition.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

---
+++ src/nim_nucleus/submodule.nim
@@ -0,0 +1,23 @@
+import ./ble/[
+  ble_client,
+  ble_gap,
+  ble_gatt_client,
+  ble_gatt_common,
+  ble_sm,
+  notifications,
+  util
+]
+import ./lib/[
+  asyncsync,
+  syslog
+]
+export
+  ble_client,
+  ble_gap,
+  ble_gatt_client,
+  ble_gatt_common,
+  ble_sm,
+  notifications,
+  util,
+  asyncsync,
+  syslog
